0d9ff20dcac89a41ca637b056112c3d2c047b054
manulangat <emmanuelthedeveloper@gmail.com>
2020-04-22 15:53:43 +0300

    fixing bugs

--- frontend/src/components/products/ProductDetal.js
+++ frontend/src/components/products/ProductDetal.js
@@ -24,41 +24,21 @@ class ProductDetal extends React.Component{
         return(
             <section className="detail">
                 <div className="prod">
-                    <Carousel arrows value={this.state.value}
-        onChange={this.onChange} >
+                    <div className="prod-img">
+                    <Carousel arrows value={this.state.value}  onChange={this.onChange} >
                     <img src={product.pic} alt="an image" className="img-example"/>
                     <img src={product.pic1} alt="an image" className="img-example"/>
                     <img src={product.pic2} alt="an image" className="img-example"/>
                     </Carousel>
-                    <Dots value={this.state.value} onChange={this.onChange} number={12} />
+                    <Dots value={this.state.value} onChange={this.onChange} number={3} />
+                    </div>
                  <div >
                 <div className="container">
-                    
                 <h1>{product.title}</h1>
                 {/* <h1>{pro.title}</h1> */}
                 <p>{product.price} | {product.category}</p>
                 <p>{product.description}</p>
                 </div>
-                </div>
-                <div>
-                    {
-                     product.variations &&   product.variations.map(vars => (
-                            <div key="vars.id">
-                                <h1>{vars.name}</h1>
-                                <div className="vars">
-                                {
-                                    vars.item_variations.map(iv => (
-                                        <div>
-                                            <h2>{iv.value}</h2>
-                                            <img src={iv.attachment} alt="an image" className="imgs"/>
-                                            
-                                        </div>
-                                    ))
-                                }
-                                </div>
-                            </div>
-                        ))
-                    }
                 </div>
                 </div>
             </section>

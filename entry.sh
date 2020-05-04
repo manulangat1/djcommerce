ddd06b5decdf6602cd807e7f88f6dc8000b0da35
manulangat <emmanuelthedeveloper@gmail.com>
2020-05-04 16:11:24 +0300

    fixing bugs

--- entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Starting Django-React Blueprint with Gunicorn!
-exec gunicorn com.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3
--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import auth from './auth'
-import products from './products'
-import cart from './cart'
-export default combineReducers({
-    auth,
-    products,
-    cart
-})

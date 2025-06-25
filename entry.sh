51fe47647cc45ae017a590110b40a44b74a38853
manulangat1 <emmanuelthedeveloper@gmail.com>
2025-06-25 22:30:58 +0300

    fix entry.sh file with correct format

--- entry.sh
+++ entry.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo Running collectstatic!
-# exec gunicorn com.wsgi:application \
-#     --bind 0.0.0.0:8000 \
-#     --workers 3
-exec python3 manage.py collectstatic --no-input
+
+python3 manage.py collectstatic --no-input
+
+echo "Starting CMD: $@"
+exec "$@"

a979b3ed7c64299c0316bc3cd29d30033982906d
manulangat1 <emmanuelthedeveloper@gmail.com>
2025-06-25 21:51:13 +0300

    Add workflow file

--- com/settings.py
+++ com/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend", "165.232.69.104"]
 
 
 # Application definition
